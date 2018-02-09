12618f8479688244ad05adf710a786fcdf6b3944
James O'Neill <james@jamesoneill.eu>
2018-02-09 19:38:29 +0000

    Build the navbar with one item

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>WaniKani Dashboard</title>
+    <title>WaniKani / Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body>
-
+<body class="bg-grey-lighter bg-texture min-h-screen">
+    {{-- Navbar --}}
+    <div class="bg-white shadow">
+        <ul class="flex list-reset text-center text-sm">
+            <li>
+                <a class="block no-underline py-5 text-grey-dark" href="#">
+                    <span class="border-4 border-grey-dark block flex h-14 items-center justify-center mb-2 rounded-full shadow-navbar-circle w-14">
+                        15
+                    </span>
+                    Levels
+                    <i class="fa fa-chevron-down text-2xs"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+    {{-- Searchbar --}}
+    {{-- Review Status --}}
+    {{-- Progression --}}
+    {{-- Kotoba Table Lists --}}
+    {{-- Forum Topics List --}}
+    {{-- Blog --}}
 </body>
 </html>
